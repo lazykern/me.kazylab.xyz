5412ff8911d18324b98aa3682ff3f6647d24a20f
Phusit Somboonyingsuk <phusitsom@gmail.com>
2022-12-15 00:29:39 +0700

    Add bio

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,40 @@
+import { Stack, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Home</title>
+        <title>phusitsom</title>
         <meta name="description" content="Homepage" />
       </Head>
-      <div className={styles.centercontainer}>
-        <main className={styles.main}>
-          <h1 className={styles.title}>
-            welcome to phusitsom&apos;s personal website
-          </h1>
-
-          <p>this website is currently under development, please stand by.</p>
-        </main>
-      </div>
+      <Box
+        sx={{
+          justifyContent: "center",
+          marginTop: { xs: "200px", md: "80px" },
+          padding: "0 20px",
+        }}
+      >
+        <Stack textAlign="left" spacing={10}>
+          <Stack>
+            <Typography variant="h1" fontWeight="400" color="primary">
+              Phusit Somboonyingsuk
+            </Typography>
+            <Typography variant="h3" paddingBottom="20px">
+              Computer Engineering Student, Kasetsart University
+            </Typography>
+            <Typography variant="body1" paddingBottom="20px">
+              A second year computer engineering student at Kasetsart
+              University. Interested in Data Engineering and Software
+              Engineering.
+            </Typography>
+            <Typography variant="body1" paddingBottom="20px">
+              Open to work as a Data Engineer intern.
+            </Typography>
+          </Stack>
+        </Stack>
+      </Box>
     </>
   );
 }
