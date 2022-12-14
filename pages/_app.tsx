ae06f3958ccba49079d1234bb99410dd9643dd00
Phusit Somboonyingsuk <phusitsom@gmail.com>
2022-12-14 17:28:54 +0700

    Add themes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import Layout from "../components/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
-    return (
-        <ThemeProvider attribute="class">
-            <Component {...pageProps} />
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,23 +3,24 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Home</title>
         <meta name="description" content="Homepage" />
       </Head>
+      <div className={styles.centercontainer}>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            welcome to phusitsom&apos;s personal website
+          </h1>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          welcome to phusitsom&apos;s personal website
-        </h1>
+          <p>this website is currently under development, please stand by.</p>
+        </main>
 
-        <p>this website is in currently under development, please stand by.</p>
-      </main>
-
-      <footer className={styles.footer}>
-        <p>help</p>
-      </footer>
-    </div>
+        <footer className={styles.footer}>
+          <p>help</p>
+        </footer>
+      </div>
+    </>
   );
 }
