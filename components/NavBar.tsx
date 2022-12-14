09e855145c61569c3fb838c36f356ac4e894c057
Phusit Somboonyingsuk <phusitsom@gmail.com>
2022-12-14 21:36:22 +0700

    Update navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,34 +4,21 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
-import Button from "@mui/material/Button";
-import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
+import Modal from "@mui/material/Modal";
+import Avatar from "@mui/material/Avatar";
 
 import { DarkMode, LightMode } from "@mui/icons-material";
 import { useTheme } from "next-themes";
-import { Avatar } from "@mui/material";
+import { Button } from "@mui/material";
+import { Stack } from "@mui/system";
 
-const pages = [""];
+const pages = ["links", "blog", "webring"];
 
 function NavBar() {
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
-    null
-  );
-
   const { systemTheme, theme, setTheme } = useTheme();
   const currentTheme = theme === "system" ? systemTheme : theme;
-
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
 
   const ThemeChanger = () => {
     if (currentTheme === "dark") {
@@ -67,19 +54,39 @@ function NavBar() {
     }
   };
 
+  const [modalOpen, setModalOpen] = React.useState(false);
+  const handleModalOpen = () => setModalOpen(true);
+  const handleModalClose = () => setModalOpen(false);
+
   return (
     <AppBar
       position="fixed"
-      sx={{ background: "transparent", color: "text.primary" }}
+      sx={{
+        boxShadow: "none",
+        top: { xs: "auto", md: 0 },
+        bottom: { xs: 0, md: "auto" },
+        background: "inherit",
+        color: "inherit",
+      }}
     >
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <Avatar
-            sx={{ mr: 2, borderRadius: 0 }}
-            alt="phusitsom"
-            className="invert-on-theme"
-            src="/images/icon.png"
-          />
+          <IconButton
+            href="/"
+            disableRipple
+            disableFocusRipple
+            disableTouchRipple
+          >
+            <Avatar
+              sx={{
+                mr: 2,
+                borderRadius: 0,
+              }}
+              alt="phusitsom"
+              className="invert-on-theme"
+              src="/images/icon.png"
+            />
+          </IconButton>
           <Typography
             variant="h6"
             noWrap
@@ -95,77 +102,75 @@ function NavBar() {
           >
             phusitsom
           </Typography>
+          {pages.map((page) => (
+            <Button key={page} sx={{ marginTop: "5px" }}>
+              {page}
+            </Button>
+          ))}
 
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+          <Box sx={{ flexGrow: 1 }}></Box>
+          <Box sx={{ flexGrow: 0 }}>
+            <ThemeChanger />
+          </Box>
+          <Box sx={{ flexGrow: 0, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
               aria-label="account of current user"
               aria-controls="menu-appbar"
               aria-haspopup="true"
-              onClick={handleOpenNavMenu}
+              onClick={handleModalOpen}
+              color="inherit"
+              disableRipple
+              disableFocusRipple
+              disableTouchRipple
             >
               <MenuIcon />
             </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
           </Box>
-          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontWeight: 700,
-              letterSpacing: ".2rem",
-              textDecoration: "none",
+          <Modal
+            open={modalOpen}
+            onClose={handleModalClose}
+            slotProps={{
+              backdrop: {
+                style: {
+                  background: currentTheme === "dark" ? "#000" : "#fff",
+                  opacity: 0.85,
+                },
+              },
             }}
           >
-            ps
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, display: "block" }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <ThemeChanger />
-          </Box>
+            <Box
+              sx={{
+                position: "absolute" as "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                width: 400,
+                p: 4,
+                justifyContent: "center",
+                textAlign: "center",
+              }}
+            >
+              <Stack spacing={2}>
+                {pages.map((page) => (
+                  <Button
+                    color="inherit"
+                    key={page}
+                    sx={{
+                      fontWeight: 700,
+                      fontSize: "1.5rem",
+                    }}
+                  >
+                    {page}
+                  </Button>
+                ))}
+              </Stack>
+            </Box>
+          </Modal>
         </Toolbar>
       </Container>
     </AppBar>
   );
 }
+
 export default NavBar;
