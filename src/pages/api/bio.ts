b78e95708d47f5d45e05a40e9abd546deedadf6d
Phusit Somboonyingsuk <phusitsom@gmail.com>
2022-12-16 00:49:42 +0700

    Remove apis

--- src/pages/api/bio.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { bioData } from "../../data";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "GET") {
-    res.status(200).json(bioData);
-  }
-}
--- src/pages/api/skills.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { skillsData } from "../../data";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "GET") {
-    res.status(200).json(skillsData);
-  }
-}
