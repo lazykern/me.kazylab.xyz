f8beedff3a58c3e09220b1e065422f83ce2a9097
Phusit Somboonyingsuk <phusitsom@gmail.com>
2022-12-15 22:37:24 +0700

    Import important css for notion

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,10 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import Layout from "../components/Layout";
 
+import "../styles/notion.css";
+import "../styles/prism.css";
+import "katex/dist/katex.min.css";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
