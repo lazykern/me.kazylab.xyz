07bb847f4e7d0d1be7a8762290a2392e9912e8a4
Phusit Somboonyingsuk <phusitsom@gmail.com>
2022-12-15 13:31:02 +0700

    Refactor apis

--- src/pages/api/skills.ts
+++ src/pages/api/skills.ts
@@ -1,34 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
-
-export type Skills = {
-  type: string;
-  skills: string[];
-};
-
-export type SkillsData = Skills[];
-
-const skills: SkillsData = [
-  {
-    type: "Core",
-    skills: ["Data Analysis", "Data Engineering", "Cloud Computing"],
-  },
-  {
-    type: "Languages",
-    skills: [
-      "Python",
-      "JavaScript/TypeScript",
-      "Rust",
-      "C",
-      "C++",
-      "Java",
-      "SQL",
-      "HTML/CSS",
-    ],
-  },
-];
+import { skillsData } from "../../data";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "GET") {
-    res.status(200).json(skills);
+    res.status(200).json(skillsData);
   }
 }
