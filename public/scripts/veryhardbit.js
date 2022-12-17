44b5fe7400419d63206ac6845b0ae8c08abc1fb6
Phusit Somboonyingsuk <phusitsom@gmail.com>
2022-12-18 00:41:35 +0700

    Some fun

---
+++ public/scripts/veryhardbit.js
@@ -0,0 +1,18 @@
+const apply = () => {
+  Array.prototype.forEach.call(
+    document.getElementsByClassName("medium-zoom-image"),
+    function(element) {
+      if (element.alt.includes("VeryHardBit")) {
+        console.log("Don't zoom in on the TuaAraiMaiRoo icon!");
+        element.addEventListener("click", function() {
+          var audio = new Audio(
+            "https://www.myinstants.com/media/sounds/vine-boom.mp3"
+          );
+          audio.play();
+        });
+      }
+    }
+  );
+};
+
+setTimeout(apply, 2000);
